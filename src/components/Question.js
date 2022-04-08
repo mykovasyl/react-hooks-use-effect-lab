03d9dd676ccdb90a1005df4e8abe44bb413a5fee
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-04-08 12:42:03 -0400

    commit tree ec03eecae90cb0b4e12167221a8696a6c176e671

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+
+    const setInterval = setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1)
+    }, 1000);
+
+    if (timeRemaining === 0) {
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+    
+      return () => clearTimeout(setInterval)
+
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
